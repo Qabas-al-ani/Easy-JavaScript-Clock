856d73ca9bbe248b66eeb4e91a188461cb11182d
Qabas al ani <doctorqa@yahoo.com>
2021-10-21 22:58:18 -0400

    created index.js to contain the functionality

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const deg
+const deg = 6;
+const hour = document.querySelector("#hr");
+const minutes = document.querySelector("#min");
+const seconds = document.querySelector("#sec");
+
+
+let day = new.Date();
+
