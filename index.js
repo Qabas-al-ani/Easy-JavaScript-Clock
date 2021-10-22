5f713d24ff9aebf0576594b7d3af56681fafb195
Qabas al ani <doctorqa@yahoo.com>
2021-10-22 00:29:14 -0400

    fixed a bug in my code

--- index.js
+++
@@ -1,8 +0,0 @@
-const deg = 6;
-const hour = document.querySelector("#hr");
-const minutes = document.querySelector("#min");
-const seconds = document.querySelector("#sec");
-
-
-let day = new.Date();
-
